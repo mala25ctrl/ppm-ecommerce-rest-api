672c0421c3edf76a7385070b8e5c895c649f1961
mala25ctrl <l.malavolti.proget@gmail.com>
2026-06-28 16:22:54 +0200

    feat: Initialize users and store apps with basic structure

---
+++ users/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = 'ADMIN', 'Admin'
+        MANAGER = 'MANAGER', 'Manager'
+        CUSTOMER = 'CUSTOMER', 'Customer'
+
+    role = models.CharField(max_length=10, choices=Role.choices, default=Role.CUSTOMER)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
