a8e75e3cfb42b3e00d166941b0590d8c12b1810a
mala25ctrl <l.malavolti.proget@gmail.com>
2026-07-09 16:04:07 +0200

    feat: Add DEFAULT_RENDERER_CLASSES to settings for JSON response support

--- core/settings.py
+++ core/settings.py
@@ -140,7 +140,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
